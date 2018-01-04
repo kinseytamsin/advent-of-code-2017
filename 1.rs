e8a7097c7dba3b85f518b6246ec2e14359054fd2
Kinsey Favre <iyunkateus@gmail.com>
2018-01-04 11:46:07 -0600

    Refactor Day 1 solution

--- 1.rs
+++ 1.rs
@@ -1,28 +1,12 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn matching_digits_sum(puzzle_input: &str) -> u32 {
+fn matching_digits_sum(puzzle_input: &str, compare_digit_distance: usize) -> u32 {
     let input_digits_iter = puzzle_input.chars().cycle();
     let mut sum = 0;
 
     for (i, j) in input_digits_iter.clone()
-                  .zip(input_digits_iter.skip(1))
-                  .take(puzzle_input.len()) {
-        let i = i.to_digit(10).unwrap();
-        let j = j.to_digit(10).unwrap();
-        if i == j {
-            sum += i;
-        }
-    }
-    sum
-}
-
-fn halfway_sum(puzzle_input: &str) -> u32 {
-    let input_digits_iter = puzzle_input.chars().cycle();
-    let mut sum = 0;
-
-    for (i, j) in input_digits_iter.clone()
-                  .zip(input_digits_iter.skip(puzzle_input.len() / 2))
+                  .zip(input_digits_iter.skip(compare_digit_distance))
                   .take(puzzle_input.len()) {
         let i = i.to_digit(10).unwrap();
         let j = j.to_digit(10).unwrap();
@@ -38,6 +22,6 @@ fn main() {
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("error reading file");
     let contents = contents.trim();
-    println!("{}", matching_digits_sum(contents));
-    println!("{}", halfway_sum(contents));
+    println!("{}", matching_digits_sum(contents, 1));
+    println!("{}", matching_digits_sum(contents, contents.len() / 2));
 }
