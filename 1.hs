8bfffcb1118d2255778d44fa061426fd8d309594
Kinsey Favre <iyunkateus@gmail.com>
2018-08-31 01:19:24 -0500

    Refactor Day 1 Haskell solution

--- 1.hs
+++ 1.hs
@@ -1,27 +1,23 @@
+import Data.Char
+import Data.Monoid
 import System.IO
 
 neighborDigits :: Int -> [Int] -> [(Int, Int)]
-neighborDigits neighborDistance xs = take (length xs)
-    $ zip (cycle xs) (drop neighborDistance $ cycle xs)
+neighborDigits n = take <$> length <*> (zip <$> cycle <*> (drop n . cycle))
 
-matchingDigitsSum :: Int -> [Int] -> Int
-matchingDigitsSum neighborDistance = foldl (+) 0
-                                     . map fst
-                                     . filter (\x -> fst x == snd x)
-                                     . neighborDigits neighborDistance
+matchingDigitsSum n = getSum . foldMap Sum
+                      . map fst
+                      . filter (uncurry (==))
+                      . neighborDigits n
 
-charToString :: Char -> String
-charToString c = [c]
+digits = map digitToInt
 
-digits :: String -> [Int]
-digits x = map (read::String->Int) $ map charToString x
+putStrLnShow = putStrLn . show
 
 main = do
-    hdl <- openFile "1.txt" ReadMode
-    contents <- hGetLine hdl
-    putStrLn $ show $ matchingDigitsSum 1 (digits contents)
-    putStrLn $ show
-        $ matchingDigitsSum
-            ((length $ digits contents) `quot` 2)
-            (digits contents)
-    hClose hdl
+    withFile "1.txt" ReadMode (\hdl -> do
+        inputDigits <- fmap digits $ hGetLine hdl
+        let inputMatchingDigitsSum = flip matchingDigitsSum $ inputDigits
+        let putSum = putStrLnShow . inputMatchingDigitsSum
+        putSum 1
+        putSum $ (length inputDigits) `quot` 2)
